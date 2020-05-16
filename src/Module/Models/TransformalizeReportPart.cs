370d311839a196effe926b232f775cd2bb8fa09a
Dale Newman <dalenewman@gmail.com>
2020-05-16 16:09:18 -0400

    initialize part properties to avoid object reference not found error

--- src/Module/Models/TransformalizeReportPart.cs
+++ src/Module/Models/TransformalizeReportPart.cs
@@ -3,6 +3,10 @@ using Module.Fields;
 
 namespace Module.Models {
    public class TransformalizeReportPart : ContentPart {
+      public TransformalizeReportPart() {
+         Arrangement = new TransformalizeArrangementField();
+         PageSizes = new PageSizesField();
+      }
       public TransformalizeArrangementField Arrangement { get; set; }
       public PageSizesField PageSizes { get; set; }
    }
--- src/Module/Models/TransformalizeTaskPart.cs
+++ src/Module/Models/TransformalizeTaskPart.cs
@@ -3,6 +3,10 @@ using Module.Fields;
 
 namespace Module.Models {
    public class TransformalizeTaskPart : ContentPart {
+
+      public TransformalizeTaskPart() {
+         Arrangement = new TransformalizeArrangementField();
+      }
       public TransformalizeArrangementField Arrangement { get; set; }
    }
 }
