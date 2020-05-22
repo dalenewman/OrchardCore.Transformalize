89eec4a3109436de320a4a6d777fc1657ea035a5
Dale Newman <dalenewman@gmail.com>
2020-05-22 09:19:43 -0400

    - updated recipe to generate new id every time - added process and entity to task log

--- src/Site/Program.cs
+++ src/Site/Program.cs
@@ -5,23 +5,19 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using OrchardCore.Logging;
 
-namespace Site
-{
-    public class Program
-    {
-        public static Task Main(string[] args)
-            => BuildHost(args).RunAsync();
+namespace Site {
+   public class Program {
+      public static Task Main(string[] args) => BuildHost(args).RunAsync();
 
-        public static IHost BuildHost(string[] args)
-        {
-            var host = Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(logging => logging.ClearProviders())
-                .ConfigureWebHostDefaults(webBuilder => webBuilder
-                        .UseNLogWeb()
-                        .UseStartup<Startup>())
-                .Build();
+      public static IHost BuildHost(string[] args) {
+         var host = Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(logging => logging.ClearProviders())
+            .ConfigureWebHostDefaults(webBuilder => webBuilder
+               .UseNLogWeb()
+               .UseStartup<Startup>())
+            .Build();
 
-            return host;
-        }
-    }
+         return host;
+      }
+   }
 }
