3f5aedd882a8bd7f00f9d87f3085e75d17ce3bcb
Dale Newman <dalenewman@gmail.com>
2020-07-24 14:39:47 -0400

    reorganize some things in the app_data samples folder

--- src/OrchardCore.Transformalize/Models/TransformalizeReportPart.cs
+++ src/OrchardCore.Transformalize/Models/TransformalizeReportPart.cs
@@ -22,11 +22,23 @@ namespace TransformalizeModule.Models {
          PageSizes = new PageSizesField();
          BulkActions = new BooleanField();
          BulkActionValueField = new TextField();
+         BulkActionCreateTask = new TextField();
+         BulkActionWriteTask = new TextField();
+         BulkActionSummaryTask = new TextField();
+         BulkActionRunTask = new TextField();
+         BulkActionSuccessTask = new TextField();
+         BulkActionFailTask = new TextField();
       }
       public TransformalizeArrangementField Arrangement { get; set; }
       public PageSizesField PageSizes { get; set; }
       public BooleanField BulkActions { get; set; }
       public TextField BulkActionValueField { get; set; }
+      public TextField BulkActionCreateTask { get; set; }
+      public TextField BulkActionWriteTask { get; set; }
+      public TextField BulkActionSummaryTask { get; set; }
+      public TextField BulkActionRunTask { get; set; }
+      public TextField BulkActionSuccessTask { get; set; }
+      public TextField BulkActionFailTask { get; set; }
 
    }
 }
