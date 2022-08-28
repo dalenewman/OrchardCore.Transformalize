bb608a688290aef020c9703fdb981d28b5b7750f
Dale Newman <dalenewman@gmail.com>
2022-08-28 11:33:55 -0400

    update libraries to fix old reference bug

--- nuget/myget.push.bat
+++ nuget/myget.push.bat
@@ -1,2 +1,2 @@
 REM - package is generated on build
-nuget push "..\src\OrchardCore.Transformalize\bin\Release\OrchardCore.Transformalize.0.10.9-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "..\src\OrchardCore.Transformalize\bin\Release\OrchardCore.Transformalize.0.10.10-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
