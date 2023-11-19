472777dc76ba0eddbac3f4c42ef8bcb4a01e7b5d
Dale Newman <dalenewman@gmail.com>
2023-11-18 20:16:28 -0500

    .net 8 and multi-architecture docker images

--- docker.sh
+++ docker.sh
@@ -2,9 +2,9 @@
 
 # docker buildx create --driver=docker-container --name=container
 
-version="0.11.0"
+version="0.11.1"
 build="mcr.microsoft.com/dotnet/sdk:8.0"
-base="mcr.microsoft.com/dotnet/aspnet:7.0"
+base="mcr.microsoft.com/dotnet/aspnet:8.0"
 name="transformalize.orchard"
 
 # docker build -f "./src/Site/Dockerfile" \
@@ -23,14 +23,13 @@ docker buildx build --builder=container \
   -f "./src/Site/Dockerfile" \
   --force-rm \
   -t dalenewman/$name:$version \
+  -t dalenewman/$name:latest \
   --build-arg BASE_IMAGE=$base \
   --build-arg BUILD_IMAGE=$build \
   --push .
 
-# -t dalenewman/$name:latest \
-
-# build="mcr.microsoft.com/dotnet/sdk:7.0-alpine"
-# base="mcr.microsoft.com/dotnet/aspnet:7.0-alpine"
+build="mcr.microsoft.com/dotnet/sdk:8.0-alpine"
+base="mcr.microsoft.com/dotnet/aspnet:8.0-alpine"
 
 # docker build -f "./src/Site/Dockerfile" \
 #   --force-rm \
@@ -43,12 +42,12 @@ docker buildx build --builder=container \
 # docker push dalenewman/$name:$version-alpine
 # docker push dalenewman/$name:latest-alpine
 
-# docker buildx build --builder=container \
-#   --platform=linux/amd64,linux/arm64 \
-#   -f "./src/Site/Dockerfile" \
-#   --force-rm \
-#   -t dalenewman/$name:$version-alpine \
-#   -t dalenewman/$name:latest-alpine \
-#   --build-arg BASE_IMAGE=$base \
-#   --build-arg BUILD_IMAGE=$build \
-#   --push .
+docker buildx build --builder=container \
+  --platform=linux/amd64,linux/arm64 \
+  -f "./src/Site/Dockerfile" \
+  --force-rm \
+  -t dalenewman/$name:$version-alpine \
+  -t dalenewman/$name:latest-alpine \
+  --build-arg BASE_IMAGE=$base \
+  --build-arg BUILD_IMAGE=$build \
+  --push .
