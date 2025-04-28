9c362d53c68a0d96a7cf44a4ba50b1f5f3413285
Dale Newman <dalenewman@gmail.com>
2025-04-27 20:39:48 -0400

    try inline map to manipulate automatic map display

--- src/OrchardCore.Transformalize/ViewModels/ParameterViewModel.cs
+++ src/OrchardCore.Transformalize/ViewModels/ParameterViewModel.cs
@@ -2,8 +2,9 @@ using System.Collections.Generic;
 using Transformalize.Configuration;
 
 namespace TransformalizeModule.ViewModels {
-    public class ParameterViewModel {
-        public IEnumerable<Map> Maps { get; set; }
-        public Parameter Parameter { get; set; }
-    }
+   public class ParameterViewModel {
+      public IEnumerable<Map> Maps { get; set; }
+      public Parameter Parameter { get; set; }
+      public Field Field { get; set; }
+   }
 }
