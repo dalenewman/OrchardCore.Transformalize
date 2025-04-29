773421c650ed63e176ce606bd1a32259874b0324
Dale Newman <dalenewman@gmail.com>
2025-04-28 22:09:28 -0400

    fix some postgresql facet bugs

--- src/OrchardCore.Transformalize/ViewModels/ParameterViewModel.cs
+++ src/OrchardCore.Transformalize/ViewModels/ParameterViewModel.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using Transformalize.Configuration;
 
 namespace TransformalizeModule.ViewModels {
@@ -6,5 +5,21 @@ namespace TransformalizeModule.ViewModels {
       public IEnumerable<Map> Maps { get; set; }
       public Parameter Parameter { get; set; }
       public Field Field { get; set; }
+
+      /// <summary>
+      /// The only use case I can think of for this is to make some modifications to the automatic facet(s)
+      /// For example, if it's a bool facet, then you can change True/true to Yes/On and False/false to No/Off or whatever
+      /// </summary>
+      /// <param name="map"></param>
+      /// <param name="item"></param>
+      public void ModifyMapItem(Map? map, MapItem item) {
+         if (map != null) {
+            var from = item.From.ToString() ?? string.Empty;
+            var mappedItem = map.Items.FirstOrDefault(i => from.StartsWith(i.From + " ("));
+            if (mappedItem != null) {
+               item.From = from.Replace(mappedItem.From + " (", mappedItem.To + " (");
+            }
+         }
+      }
    }
 }
